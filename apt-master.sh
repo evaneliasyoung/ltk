640f7d91f1985c4e7bf38d47dad76a3eb947e30b
Evan Elias Young <evanyoung762@gmail.com>
2021-06-18 20:26:01 -0500

    :fire: Removed date logging

--- apt-master.sh
+++ apt-master.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 ###
 # @file      apt-master.sh
 # @brief     My master apt command.
 #
 # @author    Evan Elias Young
 # @date      2013-10-07
-# @date      2020-10-12
-# @copyright Copyright 2020 Evan Elias Young. All rights reserved.
+# @date      2021-06-18
+# @copyright Copyright 2020-2021 Evan Elias Young. All rights reserved.
 ###
 
 which apt-get &> /dev/null
@@ -16,5 +16,3 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y autoremove
 sudo apt-get -y autoclean
-
-echo $(date +%s) > .lapt
--- pac-master.sh
+++ pac-master.sh
@@ -5,8 +5,8 @@
 #
 # @author    Evan Elias Young
 # @date      2020-10-12
-# @date      2020-10-12
-# @copyright Copyright 2020 Evan Elias Young. All rights reserved.
+# @date      2021-06-18
+# @copyright Copyright 2020-2021 Evan Elias Young. All rights reserved.
 ###
 
 which pacman &> /dev/null
@@ -14,5 +14,3 @@ if [ ! $? -eq 0 ]; then echo "failed to locate pacman installation"; exit 1; fi
 
 sudo pacman -Syy --noconfirm
 sudo pacman -Su --noconfirm
-
-echo $(date +%s) > .lpac
