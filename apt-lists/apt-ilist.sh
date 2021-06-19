dafbf7a721f8f40c5b4925f9c55cec40b4d89b26
Evan Elias Young <evanyoung762@gmail.com>
2021-06-18 20:42:46 -0500

    :sparkles: Added apt-get list installer

---
+++ apt-lists/apt-ilist.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+###
+# @file      apt-ilist.sh
+# @brief     Installs a list of apt packages.
+#
+# @author    Evan Elias Young
+# @date      2021-06-18
+# @date      2021-06-18
+# @copyright Copyright 2021 Evan Elias Young. All rights reserved.
+###
+
+die() {
+    echo "$1";
+    exit 1;
+}
+
+which apt-get &> /dev/null
+if [ ! $? -eq 0 ]; then die "failed to locate apt installation"; fi
+
+if [ -z "$1" ]; then die "usage: install_list.sh <list>"; fi
+if [ ! -f "$1" ]; then die "failed to find the package listing file"; fi
+
+xargs sudo apt-get install < "$1"
